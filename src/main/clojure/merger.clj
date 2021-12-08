042226260659ab18a65058cf594c97e32c179df2
Jan Kockrow <jan.kockrow@idealo.de>
2021-12-08 20:59:25 +0100

    chore: add simple merge logic

--- src/main/clojure/merger.clj
+++ src/main/clojure/merger.clj
@@ -1,4 +1,61 @@
-(ns merger)
+(ns merger
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io])
+  (:import (java.util.zip ZipFile)))
 
-(defn append-by-index [baseline extension-line-map merge-index]
-  (concat baseline (get extension-line-map (nth baseline merge-index))))
+(defn padr [n coll & [val]]
+  (if (<= n (count coll))
+    coll
+    (take n (concat coll (repeat (or val ""))))))
+
+(defn padl [n coll & [val]]
+  (concat (take (- n (count coll)) (repeat (or val ""))) coll))
+
+(defn chunk-map [records key-col offset size]
+  (let [chunk (take size (drop offset records))]
+    (into {} (map (fn [[key value]] [key (first value)]) (group-by #(nth % key-col) chunk)))))
+
+(defn real-merge [base-line extension-line size]
+  (map #(if (empty? %1) %2 %1) (padr size base-line) (padl size extension-line)))
+
+(defn merge-chunk [base-lines extension-line-map merge-index size]
+  (map #(real-merge % (get extension-line-map (nth % merge-index)) size) base-lines)
+  )
+
+(defn sorted-streams [zip-file]
+  (as-> (enumeration-seq (.entries zip-file)) $
+        (sort-by #(.getName %) $)
+        (map #(.getInputStream zip-file %) $)))
+
+(defn xxx [base-stream ext-stream out-stream size]
+  (with-open [base-rdr (io/reader base-stream)
+              ext-rdr (io/reader ext-stream)
+              writer (io/writer out-stream)]
+    (let [base-lines (csv/read-csv base-rdr)
+          ext-lines (csv/read-csv ext-rdr)]
+      (write-csv writer)
+      (doall
+        (merge-chunk base-lines (chunk-map ext-lines 0 0 4) 0 size)))
+    )
+
+  )
+
+
+(defn walkzip [from to]
+  (with-open [z (ZipFile. from)
+              r (File. to)]
+    (let [streams (sorted-streams z)]
+      (with-open [base-rdr (io/reader (first streams))
+                  ext-rdr (io/reader (nth streams 1))
+                  writer (io/writer to)]
+        (let [base-lines (csv/read-csv base-rdr)
+              ext-lines (csv/read-csv ext-rdr)
+              size (+ (count (first base-lines)) (count (first ext-lines)))]
+          (doall
+            (merge-chunk base-lines (chunk-map ext-lines 0 0 4) 0 size)))
+        )
+      )))
+
+
+
+
--- src/test/clojure/merger_test.clj
+++ src/test/clojure/merger_test.clj
@@ -2,14 +2,37 @@
   (:require [clojure.test :refer :all]
             [merger :refer :all]))
 
-(deftest append-by-index_appends-matching-line-from-map-to-baseline
-  (let [expected ["foo" "key" "bar" "key" "b" "c"]
-        baseline ["foo" "key" "bar"]
-        input-map {"key" ["key" "b" "c"]}]
-    (is (= (append-by-index baseline input-map 1) expected))))
+(deftest padr_pads-right
+  (is (= '(1 2 3 0) (padr 4 [1 2 3] 0)))
+  (is (= '(1 2 3 4) (padr 4 [1 2 3 4] 0)))
+  (is (= '(1 2 3 "") (padr 4 [1 2 3])))
+  (is (= '(1 2 3) (padr 1 [1 2 3]))))
 
-(deftest append-by-index_appends-nothing-without-match
-  (let [expected ["foo" "key" "bar"]
-        baseline ["foo" "key" "bar"]
-        input-map {"no-match" ["key" "b" "c"]}]
-    (is (= (append-by-index baseline input-map 1) expected))))
+(deftest padl_pads-left
+  (is (= '(0 1 2 3) (padl 4 [1 2 3] 0)))
+  (is (= '(1 2 3 4) (padl 4 [1 2 3 4] 0)))
+  (is (= '("" 1 2 3) (padl 4 [1 2 3])))
+  (is (= '(1 2 3) (padl 1 [1 2 3])))
+  )
+
+(deftest chunk-map_takes-specified-number-of-lines-at-specified-index-and-returns-a-map-with-the-specified-index-as-key
+  (let [source   [["1" "2" "3"] ["a" "b" "c"] ["4" "5" "6"] ["d" "e" "f"]]
+        group-index 1]
+    (is (= {"2" ["1" "2" "3"]
+            "b" ["a" "b" "c"]} (chunk-map source group-index 0 2)))
+    (is (= {"b" ["a" "b" "c"]
+            "5" ["4" "5" "6"]
+            "e" ["d" "e" "f"]} (chunk-map source group-index 1 3)))))
+
+(deftest merge-chunk_appends-matching-line-from-map-to-baseline
+  (let [expected [["foo" "key" "bar" "key" "b" "c"]]
+        base-lines [["foo" "key" "bar"]]
+        input-map {"key" ["key" "b" "c"]}]
+    (is (= expected (merge-chunk base-lines input-map 1 6)))))
+
+(deftest merge-chunk_appends-empty-without-match
+  (let [expected [["foo" "key" "bar" "" "" ""]]
+        base-lines [["foo" "key" "bar"]]
+        input-map {"no-match" ["no-match" "b" "c"]}]
+    (is (= expected (merge-chunk base-lines input-map 1 6)))))
+
